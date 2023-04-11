faf94ff79bf740807c4ef07f90a4c9b37123b11c
H2203 <HS1145@live.mdx.ac.uk>
2023-04-11 12:05:40 +0400

    added comments

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -31,8 +31,8 @@ export default {
      canAddToCart(products) {
       return this.$parent.canAddToCart(products); //This method calls the canAddToCart method of the parent component, which checks if the product is already in the cart and if there is enough inventory to add the product.
     },
-     addToCart(products) {
-      this.$emit("addProduct", products);      //this method emits an event called "addProduct" with the products parameter. This event is then listened to by the parent component, which can then update the cart accordingly.
+     addToCart(products) {  //this method emits an event called "addProduct" with the products parameter. This event is then listened to by the parent component, which can then update the cart accordingly.
+      this.$emit("addProduct", products);      
     },
    
   },
